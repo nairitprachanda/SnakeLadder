791370bdd76182ca6441ac26883e02fff650bdd2
nairitprachanda <nairitprachanda@gmail.com>
2020-10-02 00:12:25 +0530

    [nairit] Refactor: Position 0

---
+++ Snake_Ladder/Snake_Ladder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Snake_Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pos = 0;
+            Console.WriteLine("Position: " +pos);
+        }
+    }
+}
