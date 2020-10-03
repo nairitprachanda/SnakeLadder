d87783bfcdcda94a88646a71b159cbf882bb2f9f
nairitprachanda <nairitprachanda@gmail.com>
2020-10-03 11:42:01 +0530

    [nairit] Refactor: Rolling till winning

--- Snake_Ladder/Snake_Ladder/Program.cs
+++ Snake_Ladder/Snake_Ladder/Program.cs
@@ -7,33 +7,36 @@ namespace Snake_Ladder
         static void Main(string[] args)
         {
             int pos = 0;
-            Console.WriteLine("Position: " +pos);
-            Random random = new Random();
-            int dieNum = random.Next(1, 7);
-            Console.WriteLine("Number after rolling die: " + dieNum);
-            Console.WriteLine("Three options - No Play, Ladder, Snake ");
-            int option = random.Next(0, 3);
+            Console.WriteLine("Starting  Position :  " + pos);
             const int NoPlay = 0;
             const int Ladder = 1;
             const int Snake = 2;
-            switch (option)
+            const int WinPosition = 100;
+            int count = 0;
+            while (pos < WinPosition)
             {
-                case NoPlay:
-                    Console.WriteLine("No Play");
-                    break;
-                case Ladder:
-                    Console.WriteLine("Ladder");
-                    pos = pos + dieNum;
-                    break;
-                case Snake:
-                    Console.WriteLine("Snake");
-                    if (pos - dieNum > 0)
-                        pos = pos - dieNum;
-                    else
-                        pos = 0;
-                    break;
+                Random random = new Random();
+                int dieNum = random.Next(1, 7);
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case NoPlay:
+                        break;
+                    case Ladder:
+                        pos = pos + dieNum;
+                        break;
+                    case Snake:
+                        if (pos - dieNum > 0)
+                            pos = pos - dieNum;
+                        else
+                            pos = 0;
+                        break;
+                }
+                count++;
             }
-            Console.WriteLine("The New Position :  " + pos);
+            Console.WriteLine("New Position :  " + pos);
+            Console.WriteLine("The Player Has Won !! ");
+            Console.WriteLine("After rolling the die '" + count + "' times");
 
         }
     }
