4540426ab1a39f75f845b206b342e4154d489e5a
nairitprachanda <nairitprachanda@gmail.com>
2020-10-03 11:26:57 +0530

    [nairit] Refactor: Displaying die number

--- Snake_Ladder/Snake_Ladder/Program.cs
+++ Snake_Ladder/Snake_Ladder/Program.cs
@@ -8,6 +8,10 @@ namespace Snake_Ladder
         {
             int pos = 0;
             Console.WriteLine("Position: " +pos);
+            Random random = new Random();
+            int dieNum = random.Next(1, 7);
+            Console.WriteLine("Number after rolling die: " + dieNum);
+
         }
     }
 }
